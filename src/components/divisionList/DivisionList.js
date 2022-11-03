28e80b67ca2d64b53e7ad6ee2831ab1a9a9bb3f1
CWIN77 <cwstbp0426@gmail.com>
2022-11-03 21:27:51 +0900

    UPDATE:관리 부서 페이지 제작 완료

--- src/components/divisionList/DivisionList.js
+++ src/components/divisionList/DivisionList.js
@@ -1,10 +1,46 @@
 import * as S from './DivisionListStyle';
-
 export default function DivisionList() {
   return (
     <S.Container>
+      <S.Header>
+        <S.Title>With Office</S.Title>
+      </S.Header>
       <S.ContentWrap>
-
+        <S.DivListWrap>
+          <S.DivList>
+            <S.Division style={{ backgroundColor: "#E9E9E9", alignItems: "center", justifyContent: "center" }}>
+              <S.AddDiv>+ 개발 부서 추가</S.AddDiv>
+            </S.Division>
+          </S.DivList>
+          {
+            <S.DivList>
+              <S.Division>
+                <S.DivTitle>개발 부서 1팀</S.DivTitle>
+                <div>
+                  <S.DivInform>
+                    <S.DivInformTag>담당자</S.DivInformTag>
+                    <S.DivInformValue>홍길동</S.DivInformValue>
+                  </S.DivInform>
+                  <S.DivInform>
+                    <S.DivInformTag>메일</S.DivInformTag>
+                    <S.DivInformValue>cwstbp0426@gmail.com</S.DivInformValue>
+                  </S.DivInform>
+                </div>
+              </S.Division>
+            </S.DivList>
+          }
+        </S.DivListWrap>
+        <S.PeopleList>
+          {
+            <S.PeopleWrap>
+              <S.PeopleName>최우승</S.PeopleName>
+              <S.PeopleInformWrap>
+                <S.PeopleInform>010123412</S.PeopleInform>
+                <S.PeopleInform>example@gmail.com</S.PeopleInform>
+              </S.PeopleInformWrap>
+            </S.PeopleWrap>
+          }
+        </S.PeopleList>
       </S.ContentWrap>
     </S.Container>
   )
--- src/components/divisionList/DivisionListStyle.js
+++ src/components/divisionList/DivisionListStyle.js
@@ -2,77 +2,103 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width:100%;
-  min-height:100%;
 `
-export const BackgroundImg = styled.img`
-  width:auto;
-  height:100vh;
-  z-index: -1;
-  position: fixed;
-  filter: blur(2px);
-`
-export const BackgroundLinear = styled.div`
-  background-color: rgba(0,0,0,0.3);
+export const Header = styled.header`
   width:100%;
-  height: 100%;
-  position: fixed;
-  top: 0px;
-  z-index: -1;
+  background-color: #BBDEFF;
 `
-export const DescriptWrap = styled.section`
+export const Title = styled.h1`
+  color: #3C3C3C;
+  font-size: 36px;
+  font-weight: bold;
+  padding:12px;
+  padding-left: 36px;
+`
+export const DivList = styled.section`
+  cursor: pointer;
+  width:calc((100% / 3) - 48px);
+  height:calc(20vw - 48px);
+  padding: 36px;
+  padding-top:24px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+`
+export const DivListWrap = styled.div`
   width:100%;
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  justify-content: flex-end;
-  margin-top: 40px;
-  margin-left: 8vw;
-`
-export const Descript = styled.h1`
-  font-size: 20px;
-  font-weight: 600;
-  margin-bottom: 6px;
-`
-export const Title = styled.strong`
-  font-size: 38px;
-  margin-bottom: 32px;
-  margin-top: 4px;
+  display:flex;
+  align-content: flex-start;
+  flex-wrap: wrap;
 `
 export const ContentWrap = styled.div`
-  width:100%;
-  min-height:100vh;
+  display:flex;
+`
+export const PeopleList = styled.div`
   display:flex;
   flex-direction: column;
-  justify-content:space-evenly;
-  align-items: center;
+  background-color: black;
+  width:500px;
+  height:calc(100vh - 60px - 40px);
+  padding: 20px 0px;
 `
-export const LoginWrap = styled.div`
+export const Division = styled.div`
+  width:calc(100% - 48px);
+  height:calc(100% - 48px);
+  padding:24px;
+  background-color:black;
+  border-radius: 12px;
   display:flex;
   flex-direction: column;
-  align-items: center;
-  height:40vh;
+  justify-content: space-between;
 `
-export const LoginText = styled.h1`
-  font-size: 20px;
-  margin-top: 12px;
-  margin-bottom: 32px;
-  font-weight: 600;
-`
-export const LoginInput = styled.input`
-  color:black;
-  padding: 10px 12px;
-  width:60vw;
-  margin-top: 10px;
-  margin-bottom: 6px;
-`
-export const LoginBtn = styled.button`
+export const DivTitle = styled.h1`
   color:white;
-  padding: 10px 24px;
-  width:calc(60vw + 26px);
-  background-color: blue;
-  outline: none;
-  border: none;
-  margin-top: 32px;
-  font-weight: 600;
-  cursor: pointer;
+  font-weight: bold;
+  font-size: 32px;
+  margin: 8px 0px;
+`
+export const DivInform = styled.div`
+  display:flex;
+  align-items: center;
+  margin-bottom: 12px;
+`
+export const DivInformTag = styled.span`
+  padding:6px 0px;
+  border-radius: 6px;
+  background-color: #14FF00;
+  color:black;
+  font-size: 15px;
+  margin-right: 12px;
+  width:60px;
+  text-align: center;
+`
+export const DivInformValue = styled.h1`
+  font-size: 16px;
+`
+export const AddDiv = styled.h1`
+  color:black;
+  font-size: 18px;
+  font-weight: bold;
+`
+export const PeopleWrap = styled.div`
+  width:calc(100% - 48px - 48px);
+  padding: 24px;
+  margin: 24px;
+  margin-bottom: 8px;
+  background-color: #192F7E;
+  display:flex;
+  justify-content: space-between;
+  border-radius: 8px;
+`
+export const PeopleName = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  width:auto;
+`
+export const PeopleInform = styled.h2`
+  font-size: 15px;
+`
+export const PeopleInformWrap = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items: flex-end;
 `
